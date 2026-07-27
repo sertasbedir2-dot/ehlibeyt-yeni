4dc58e34b666e47b879e81779f20e2e8ecfcf7aa
sertasbedir2-dot <sertasbedir2@gmail.com>
2026-07-28 00:19:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'     
 import './App.css'       
-import { HelmetProvider } from 'react-helmet-async';
-import { Analytics } from '@vercel/analytics/react'; 
+import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,8 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
       <Analytics /> 
     </HelmetProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
-
-
-
