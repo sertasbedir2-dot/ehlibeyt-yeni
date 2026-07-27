be3d54282cf06e62e795832c63053298ce9af437
sertasbedir2-dot <sertasbedir2@gmail.com>
2026-07-28 00:15:10 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,32 @@
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
 export default [
+  { 
+    // Derlenen dosyaları ve bağımlılıkları denetimden muaf tut
+    ignores: ['dist', 'node_modules', 'public'] 
+  },
   {
-    // Bu 'ignores' komutu, projedeki tüm dosyaları (**/*)
-    // denetimden muaf tutar. Kirmizi carpiya sebep olan
-    // hicbir kontrol yapilmaz.
-    ignores: ["**/*"]
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      // Sadece en kritik React ve Hook hatalarını yakala
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'warn'
+    }
   }
 ];
